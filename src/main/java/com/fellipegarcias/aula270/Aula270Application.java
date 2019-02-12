33f48a3724cbc35867d88bd39f6ae8814d735bc5
FellipeG <fellipegarciasrj@yahoo.com.br>
2019-02-12 02:20:17 -0200

    First commit - created project

---
+++ src/main/java/com/fellipegarcias/aula270/Aula270Application.java
@@ -0,0 +1,14 @@
+package com.fellipegarcias.aula270;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula270Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula270Application.class, args);
+	}
+
+}
+
