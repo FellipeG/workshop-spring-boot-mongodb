60dc8f2053eb64b7b639a5f31001dc62e0366877
FellipeG <fellipegarciasrj@yahoo.com.br>
2019-02-16 17:40:42 -0200

    Getting a post by ID

---
+++ src/main/java/com/fellipegarcias/aula270/resources/PostResource.java
@@ -0,0 +1,31 @@
+package com.fellipegarcias.aula270.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fellipegarcias.aula270.domain.Post;
+import com.fellipegarcias.aula270.services.PostService;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService service;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findAll() {
+		return ResponseEntity.ok().body(service.findAll());
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		return ResponseEntity.ok().body(service.findById(id));
+	}
+}
---
+++ src/main/java/com/fellipegarcias/aula270/services/PostService.java
@@ -0,0 +1,26 @@
+package com.fellipegarcias.aula270.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fellipegarcias.aula270.domain.Post;
+import com.fellipegarcias.aula270.repository.PostRepository;
+import com.fellipegarcias.aula270.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public List<Post> findAll() {
+		return repo.findAll();
+	}
+	
+	public Post findById(String id) {
+		return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Can't find post with that ID"));
+	}
+
+}
