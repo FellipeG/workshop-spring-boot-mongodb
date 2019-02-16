eb9f68c22c68a27fb655327494b22707570bd22a
FellipeG <fellipegarciasrj@yahoo.com.br>
2019-02-16 01:45:21 -0200

    Endpoint to return a user posts

--- src/main/java/com/fellipegarcias/aula270/resources/UserResource.java
+++ src/main/java/com/fellipegarcias/aula270/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.fellipegarcias.aula270.domain.Post;
 import com.fellipegarcias.aula270.domain.User;
 import com.fellipegarcias.aula270.dto.UserDTO;
 import com.fellipegarcias.aula270.services.UserService;
@@ -57,5 +58,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> getPosts(@PathVariable String id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
 	}
 }
